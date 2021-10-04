b00cc98608d190cc0cd05becad5d6ac239d6ea54
mheliot <maxime.heliotpro@gmail.com>
2021-10-04 11:29:12 +0200

    Scene parameters + prefab adjustments

--- LudumDare49/Assets/Scripts/GorillaController.cs
+++ LudumDare49/Assets/Scripts/GorillaController.cs
@@ -20,21 +20,26 @@ public class GorillaController : MonoBehaviour
     public Transform leftRopeAnchor;
     public Transform rightRopeAnchor;
 
-    private List<Rigidbody2D> jointRbs;
+    private List<Rigidbody2D> _jointRbs;
+    private SpriteRenderer _gorillaSprite;
     
     private int _life;
     private float _timeBtwAttack = 0.0f;
+    private bool _isSmashing = false;
+    private bool _hasEnrage = false;
+    private bool _invincibility = false;
 
     private Animator _gorillaAnimator;
 
     private void Start()
     {
         _gorillaAnimator = GetComponent<Animator>();
+        _gorillaSprite = GetComponentInChildren<SpriteRenderer>();
         
         leftRopeAnchor = GameObject.FindGameObjectWithTag("LeftAnchor").transform;
         rightRopeAnchor = GameObject.FindGameObjectWithTag("RightAnchor").transform;
         
-        jointRbs = leftRopeAnchor.gameObject.GetComponentsInChildren<Rigidbody2D>().ToList();
+        _jointRbs = leftRopeAnchor.gameObject.GetComponentsInChildren<Rigidbody2D>().ToList();
 
         _life = maxLives;
         _timeBtwAttack = 5.0f;
@@ -43,7 +48,7 @@ public class GorillaController : MonoBehaviour
 
     private void Update()
     {
-        if (!isDead)
+        if (!isDead && !_isSmashing)
         {
             if (_timeBtwAttack <= 0)
             {
@@ -55,11 +60,38 @@ public class GorillaController : MonoBehaviour
                 _timeBtwAttack -= Time.deltaTime;
             }
         }
+
+        if (_life == 15 && !_hasEnrage)
+        {
+            _hasEnrage = true;
+            StartCoroutine(BigSmash());
+        }
+    }
+
+    IEnumerator BigSmash()
+    {
+        _invincibility = true;
+        _isSmashing = true;
+        _gorillaAnimator.SetTrigger("big_smash");
+        yield return new WaitForSeconds(2.1f);
+        
+        _gorillaAnimator.SetTrigger("instant_smash");
+        yield return new WaitForSeconds(1f);
+        
+        _gorillaAnimator.SetTrigger("instant_smash");
+        yield return new WaitForSeconds(1f);
+        
+        _gorillaAnimator.SetTrigger("instant_smash");
+        yield return new WaitForSeconds(1f);
+        
+        _timeBtwAttack = startTimeBtwAttack;
+        _isSmashing = false;
+        _invincibility = false;
     }
 
     private void ApplyForce()
     {
-        foreach (var rigidbody in jointRbs)
+        foreach (var rigidbody in _jointRbs)
         {
             if (Vector2.Distance(rigidbody.position, leftRopeAnchor.position) <=
                 Vector2.Distance(rigidbody.position, rightRopeAnchor.position))
@@ -75,8 +107,10 @@ public class GorillaController : MonoBehaviour
 
     IEnumerator Smash()
     {
+        _isSmashing = true;
         _gorillaAnimator.SetTrigger("smash");
         yield return null;
+        _isSmashing = false;
     }
 
     private void SetGorillaPosition()
@@ -97,13 +131,38 @@ public class GorillaController : MonoBehaviour
     
     public bool TakeDamage(int damage)
     {
-        _life -= damage;
-        if (_life <= 0)
+        if (!_invincibility)
         {
-            StartCoroutine(Die());
+            _life -= damage;
+            if (_life <= 0)
+            {
+                StartCoroutine(Die());
+            }
+            else
+            {
+                StartCoroutine(Damaged());
+            }
+
         }
 
         return isDead;
+    }
+    
+    IEnumerator Damaged()
+    {
+        Color normalColor = Color.white;
+        Color hitColor = Color.clear;
+
+        for (int i = 0; i < 2; i++)
+        {
+            _gorillaSprite.material.color = hitColor;
+            yield return new WaitForSeconds(0.05f);
+            _gorillaSprite.material.color = normalColor;
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        yield return null;
+        _gorillaSprite.material.color = normalColor;
     }
 
     IEnumerator Die()
